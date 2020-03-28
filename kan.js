8ed9f83b021c37cd95ae6387a6ad253f0683ffa0
ckorir <chrisruto.k@gmail.com>
2020-03-28 21:32:28 +0300

    added id to check button

---
+++ kan.js
@@ -0,0 +1,7 @@
+var day = document.getElementById("dob").value;
+var month = document.getElementById("month").value;
+var month = document.getElementById("year").value;
+
+function result(){
+    alert("hello");
+}
