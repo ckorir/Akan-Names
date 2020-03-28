e5a87c44347ccd0f6f75cde2148dbdfa04362708
ckorir <chrisruto.k@gmail.com>
2020-03-29 00:52:06 +0300

    removed variable error

--- kan.js
+++ kan.js
@@ -3,12 +3,9 @@ function validate(){
     var dd = document.getElementById("dob").value;
     var mm = document.getElementById("month").value;
     var yy = document.getElementById("year").value;
-    var
     
     if(dd.value === '' || mm === '' || yy === '' ){
         alert("Please complete the form")
     }
-    
-    
-   
+ 
 }
