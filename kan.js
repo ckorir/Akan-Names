eee78881e62e4e370fedf997dc51b2116e8e2e6a
ckorir <chrisruto.k@gmail.com>
2020-03-28 22:55:18 +0300

    created variables for month,day and year

--- kan.js
+++ kan.js
@@ -1,5 +1,20 @@
+var a;
+var b;
+var c;
 
- function result(){
-    var sub = document.getElementById("button1").value;
-    alert("hello");
+function result(){
+    var dd = document.getElementById("dob").value;
+    var mm = document.getElementById("month").value;
+    var yy = document.getElementById("year").value;
+
+    var a = parseInt(dd);
+    var b = parseInt(mm);
+    var c = parseInt(yy);
+
+var add = function(a,b,c){
+    return a + b + c;
+}
+
+var end = add(a,b,c)
+alert(dob.value + mm.value);
 }
