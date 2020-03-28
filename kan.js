d5641c431016da7e8e5b27b8e1ecb632841f9d9f
ckorir <chrisruto.k@gmail.com>
2020-03-29 00:45:21 +0300

    added validation alert

--- kan.js
+++ kan.js
@@ -1,20 +1,16 @@
-var a;
-var b;
-var c;
 
-function result(){
+function validate(){
     var dd = document.getElementById("dob").value;
     var mm = document.getElementById("month").value;
     var yy = document.getElementById("year").value;
-
-    var a = parseInt(dd);
-    var b = parseInt(mm);
-    var c = parseInt(yy);
-
-var add = function(a,b,c){
-    return a + b + c;
-}
-
-var end = add(a,b,c)
-alert(dob.value + mm.value);
+    var
+    
+    if(dd.value === '' || mm === '' || yy === '' ){
+        alert("Please complete the form")
+    }
+    else{
+        alert("Welcome");
+    }
+    
+   
 }
