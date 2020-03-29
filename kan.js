dcebb21e37ebcc8339f708259d2e4c9990ce8231
ckorir <chrisruto.k@gmail.com>
2020-03-29 17:50:32 +0300

    removed parseint from dd,mm,yy

--- kan.js
+++ kan.js
@@ -1,11 +1,11 @@
 
 
 function validate(){
-    var dd = parseInt(document.getElementById("dob").value);
-    var mm = parseInt(document.getElementById("month").value);
-    var yy = parseInt(document.getElementById("year").value);
+    var dd = document.getElementById("dob").value;
+    var mm = document.getElementById("month").value;
+    var yy = document.getElementById("year").value;
     
-    if(dd.value === '' || mm === '' || yy === '' ){
+    if(dd === '' || mm === '' || yy === '' ){
         swal({
             title: "Error",
             text: "You haven't filled all fields!",
