9034ea0b2a5c72c65daaeb77982f243c02f8786c
ckorir <chrisruto.k@gmail.com>
2020-03-29 17:54:29 +0300

    added a field for the user to enter their century

--- kan.js
+++ kan.js
@@ -4,8 +4,9 @@ function validate(){
     var dd = document.getElementById("dob").value;
     var mm = document.getElementById("month").value;
     var yy = document.getElementById("year").value;
+    var cc = document.getElementById("century").value;
     
-    if(dd === '' || mm === '' || yy === '' ){
+    if(dd === '' || mm === '' || yy === '' || cc == ''){
         swal({
             title: "Error",
             text: "You haven't filled all fields!",
