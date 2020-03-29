a19928a4f2f6e6531d1c532688fd3ada0fdb4f75
ckorir <chrisruto.k@gmail.com>
2020-03-29 18:24:26 +0300

    made output message for male born on sunday

--- kan.js
+++ kan.js
@@ -5,6 +5,8 @@ function validate(){
     var mm = document.getElementById("month").value;
     var yy = document.getElementById("year").value;
     var cc = document.getElementById("century").value;
+    var male = document.getElementById("op1").value;
+    var female = document.getElementById("op2").value;
     
     var d = parseInt(dd);
     var m = parseInt(mm);
@@ -14,6 +16,10 @@ function validate(){
     var math = (( (c/4) -2*c-1) + ((5*y/4) ) + ((26*(m+1)/10)) + d)%7;
     var exact = Math.round(math);
 
+    if(exact === 1 && op1.checked === true){
+        alert("Your Akan name is Kwasi")
+    }
+
     if(dd === '' || mm === '' || yy === '' || cc == ''){
         swal({
             title: "Error",
@@ -22,7 +28,6 @@ function validate(){
           });
     }
       
-    else{
-        alert(exact);
-    }
+    
+
 }
